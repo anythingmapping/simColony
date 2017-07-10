1f73b29b260a3848a8ded23676f1710a8e1bf2ad
LucasPNCC <lucas.mostyn@pncc.govt.nz>
2017-07-11 09:07:30 +1200

    used the update method with a L.latlng object to update marker but its not quite working

--- js/asset.js
+++ js/asset.js
@@ -71,11 +71,11 @@ redTeam = [];
 
 for(i=0; i < 10; i++){
   var mkrRed = L.marker(ctrlStartRed_latlng, {icon: icnRed}).addTo(map);
-  console.log(mkrRed);
+  // console.log(mkrRed);
   var id = "Red";
   var unique = id + i;
   var unique = mkrRed;
-  console.log(mkrRed);
+  // console.log(mkrRed);
   redTeam.push(mkrRed);
 };
 
@@ -83,20 +83,28 @@ var mkrRed = L.marker(ctrlStartRed_latlng, {icon: icnRed}).addTo(map);
 var mkrGreen = L.marker([46.109864097197146, 0.06161570549011231], {icon: icnGreen}).addTo(map);
 var mkrFood = L.marker([46.11274627144683, 0.06698548793792726], {icon: icnBlack}).addTo(map);
 var mkrGreenLoc = mkrGreen.getLatLng();
+console.log(mkrGreenLoc["lat"],mkrGreenLoc["lng"]);
+console.log(mkrGreenLoc["lat"]+0.001,mkrGreenLoc["lng"]);
 // *************** SIMULATION STARTING SETUP ************
 
 
-function greenteam() {
-  // console.log(mkrGreenLoc["lng"]);
+function greenteam(marker) {
+  // console.log(marker);
+  var coordTarget = marker.getLatLng();
+  var coord = L.latLng([46.11274627144683, 0.06698548793792726]);
+  // do something to the coords and turn it back into a lnglat
+  mkrGreen.setLatLng(coord);
+  // mkrGreen.setLatLng(mkrGreenLoc["lng"], mkrGreenLoc["lng"]);
+  // console.log(mkrGreen);
   // console.log(mkrGreenLoc["lng"]+.001);
   // console.log(loc.distanceTo(ship.getLatLng())/1000 + " km to boat");
   // console.log(mkrGreen.getLatLng["lat"]);
 
-  var mkrGreenLatX = getRandomArbitrary(mkrGreenLoc["lng"], mkrGreenLoc["lng"]);
+  // var mkrGreenLatX = getRandomArbitrary(mkrGreenLoc["lng"], mkrGreenLoc["lng"]);
   // var mkrGreenLatX = 46.109864097197146;
-  var mkrGreenLngY = 0.06161570549011231;
-  mkrGreen.setLatLng([mkrGreenLatX, mkrGreenLngY]);
-  console.log(mkrGreen.getLatLng);
+  // var mkrGreenLngY = 0.06161570549011231;
+  // mkrGreen.setLatLng([mkrGreenLatX, mkrGreenLngY]);
+  // console.log(mkrGreen.getLatLng);
   // console.log(mkrGreen.lat);
 };
 
@@ -136,7 +144,7 @@ function animFrame(){
 function onEachStep(){
   next_assetY = next_assetY +0.01;
   ship.setLatLng([next_assetX,next_assetY]);
-  greenteam();
+  greenteam(mkrGreen);
   redteam();
   // console.log(next_assetY);
 };
